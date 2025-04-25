5f7e4b2aa1b05010dc7fe23946806415ff0cbeb4
AESiR <cyborgkiller1008@gmail.com>
2025-04-25 23:30:03 +0530

    fix: update Supabase client configuration to use correct environment variables

--- lib/supabase/createClient.ts
+++ lib/supabase/createClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.DB_SUPABASE_URL!,
+  process.env.DB_NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
